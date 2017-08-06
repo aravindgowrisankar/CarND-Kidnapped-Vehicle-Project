0c2bad2d417c280eb70f91a9033c27a5c25f749b
Aravind Gowrisankar <gowrisankar@apple.com>
2017-08-06 11:36:42 -0700

    Feat: WIP Code that's not yet working

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -14,7 +14,7 @@
 #include <sstream>
 #include <string>
 #include <iterator>
-
+#include <limits>
 #include "particle_filter.h"
 
 using namespace std;
@@ -26,7 +26,7 @@ void ParticleFilter::init(double x, double y, double theta, double std_devs[]) {
 	// Add random Gaussian noise to each particle.
 	// NOTE: Consult particle_filter.h for more information about this method (and others in this file).
 
-  num_particles=1000;
+  num_particles=100;
   normal_distribution<double> dist_x(x, std_devs[0]);
   normal_distribution<double> dist_y(y, std_devs[1]);
   normal_distribution<double> dist_theta(theta, std_devs[2]);
@@ -80,33 +80,34 @@ void ParticleFilter::dataAssociation(std::vector<LandmarkObs> predicted, std::ve
 
   double num_predicted=predicted.size();
   double num_observations=observations.size();
-  //cout<<"num_predicted="<<num_predicted<<"num_observations="<<num_observations<<endl;
+  int min_j;
+  //cout<<"Num Landmarks"<<num_predicted<<" Num observations"<<num_observations;
   if (num_predicted==0)
     return;
   double min_dist;
   int landmark_index;
   for (int i=0;i<observations.size();i++) {
     //cout<<"Original observation ID "<<observations[i].id;
-    min_dist=100000.0;//arbitrary large number
+    min_dist=std::numeric_limits<double>::max();
     for (int j=0;j<predicted.size();j++) {
       double distance=dist(observations[i].x,observations[i].y,predicted[j].x,predicted[j].y);
       //cout<<"Distance between observation "<<observations[i].id<<" and landmark "<<predicted[j].id<<"="<<distance<<endl;
       if (distance<min_dist) {
         min_dist=distance;
-        observations[i].id=predicted[j].id;
+        min_j=j;
       }
       
     }
+    observations[i].id=predicted[min_j].id;
+    //cout<<"Assigned obs("<<observations[i].x<<","<<observations[i].y<<") to landmark"<<observations[i].id<<". ("<<predicted[min_j].x<<","<<predicted[min_j].y<<")"<<endl;
 
     //cout<<"Landmark for observation  "<<observations[i].id<<endl;
   }
+  //cout<<"End of data associaton"<<endl;
 
-  // for (int i=0;i<observations.size();i++) {
-  //   cout<<"Assigned observation "<<observations[i].id<<" to landmark"
-  // }
 }
 
-std::vector<LandmarkObs> transform(double particle_x, double particle_y, double  particle_theta, std::vector<LandmarkObs> observations) {
+std::vector<LandmarkObs> transform_old(double particle_x, double particle_y, double  particle_theta, std::vector<LandmarkObs> observations) {
   std::vector<LandmarkObs> transformed_observations;
   for (int i=0;i<observations.size();i++) {
     LandmarkObs tobs;
@@ -114,6 +115,19 @@ std::vector<LandmarkObs> transform(double particle_x, double particle_y, double 
     double r=observations[i].y-(observations[i].x/tan(particle_theta));
     tobs.x=particle_x-(r*sin(particle_theta));
     tobs.y=particle_y+(observations[i].x/sin(particle_theta))+(r*cos(particle_theta));
+    transformed_observations.push_back(tobs);
+    //cout<<"original ("<<observations[i].x<<","<<observations[i].y<<")"<<" transformed ("<<tobs.x<<","<<tobs.y<<")";
+  }
+  return transformed_observations;
+}
+
+std::vector<LandmarkObs> transform(double particle_x, double particle_y, double  particle_theta, std::vector<LandmarkObs> observations) {
+  std::vector<LandmarkObs> transformed_observations;
+  for (int i=0;i<observations.size();i++) {
+    LandmarkObs tobs;
+    tobs.id=observations[i].id;
+    tobs.x=particle_x+(observations[i].x*cos(particle_theta))-(observations[i].y*sin(particle_theta));
+    tobs.y=particle_y+(observations[i].x*sin(particle_theta))+(observations[i].y*cos(particle_theta));
     transformed_observations.push_back(tobs);
     //cout<<"original ("<<observations[i].x<<","<<observations[i].y<<")"<<" transformed ("<<tobs.x<<","<<tobs.y<<")";
   }
@@ -130,9 +144,12 @@ double update_particle_weight(std::vector<LandmarkObs> observations,Map map_land
 
   for (int i=0;i<observations.size();i++) {
     int map_index=observations[i].id-1;
-
+    
     double x=map_landmarks.landmark_list[map_index].x_f;
     double y=map_landmarks.landmark_list[map_index].y_f;
+    int id=map_landmarks.landmark_list[map_index].id_i;
+    if (id!=observations[i].id)
+      cout<<"Exception Map id"<<id<<" is not the same as tobs id"<<observations[i].id;
     double x_comp=-(observations[i].x-x)*(observations[i].x-x)/(2*sigma_x_sq);
     double y_comp=-(observations[i].y-y)*(observations[i].y-y)/(2*sigma_y_sq);
     //cout<<"Map index"<<map_index<<"location "<<x<<","<<y<<"observation "<<observations[i].x<<","<<observations[i].y;
@@ -144,9 +161,9 @@ double update_particle_weight(std::vector<LandmarkObs> observations,Map map_land
 
 void ParticleFilter::updateWeights(double sensor_range, double std_landmark[], 
 		std::vector<LandmarkObs> observations, Map map_landmarks) {
-  // cout<<"Map size"<<map_landmarks.landmark_list.size();
-  // cout<<"Obs size"<<observations.size();
-  // cout<<"Sensor range"<<sensor_range;
+
+  cout<<"Obs size"<<observations.size();
+  cout<<"Sensor range"<<sensor_range<<endl;
   for (int i=0;i<num_particles;i++) {
     std::vector<LandmarkObs> transformed_obs=transform(particles[i].x, particles[i].y, particles[i].theta,observations);
     std::vector<LandmarkObs> map_landmarks_subset;
@@ -196,16 +213,28 @@ void ParticleFilter::resample() {
   // TODO: Resample particles with replacement with probability proportional to their weight. 
   // NOTE: You may find std::discrete_distribution helpful here.
   //   http://en.cppreference.com/w/cpp/numeric/random/discrete_distribution
+  double highest_weight = -1.0;
+  double weight_sum = 0.0;
+  for (int i = 0; i < num_particles; ++i) {
+    if (particles[i].weight > highest_weight) {
+      highest_weight = particles[i].weight;
+    }
+    weight_sum += particles[i].weight;
+  }
+  cout << "In Resample: before  highest w " << highest_weight << endl;
+  cout << "In Resample: before  average w " << weight_sum/num_particles << endl;  
+
+
   std::random_device rd;
   std::mt19937 gen(rd());
   std::vector<Particle> new_particles;
   std::discrete_distribution<double> d(weights.begin(), weights.end());
-  weights.clear();
+  //weights.clear();
   //double sum = std::accumulate(weights.begin(), weights.end(), 0.0);
   //cout<<"sum="<<sum<<endl;
   for (int i=0;i<num_particles;i++){
     int selected=d(gen);
-    cout <<"selected particle"<<selected;
+    //cout <<"selected particle"<<selected;
     Particle p;
     p.id=i;
     p.x=particles[selected].x;
@@ -216,7 +245,17 @@ void ParticleFilter::resample() {
   }
   particles.clear();
   particles=new_particles;
-  
+  highest_weight = -1.0;
+  weight_sum = 0.0;
+  for (int i = 0; i < num_particles; ++i) {
+    if (particles[i].weight > highest_weight) {
+      highest_weight = particles[i].weight;
+    }
+    weight_sum += particles[i].weight;
+    cout<<"Particle "<<i<<" ("<<particles[i].x<<","<<particles[i].y<<")"<<endl;
+  }
+  cout << "Resample highest w " << highest_weight << endl;
+  cout << "Resample average w " << weight_sum/num_particles << endl;  
 }
 
 Particle ParticleFilter::SetAssociations(Particle particle, std::vector<int> associations, std::vector<double> sense_x, std::vector<double> sense_y)
